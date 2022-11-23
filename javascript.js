d99a3be9586c0b56363f07c030a3f05816475dea
JovanChang <jovanchang19@gmail.com>
2022-11-23 02:47:21 +0000

    added slideshow and youtube player

--- javascript.js
+++ javascript.js
@@ -3,3 +3,33 @@ document.getElementById('press').addEventListener('click', Text);
 function Text() {
     document.getElementById('hiddenText').innerHTML = 'Wow this paragraph is not hidden anymore';
 }
+
+//slideshow function from w3school: https://www.w3schools.com/howto/howto_js_slideshow.asp
+let slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
