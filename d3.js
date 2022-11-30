9dbadc6e0041b5a69917238d467e183ef3b2144f
JovanChang <jovanchang19@gmail.com>
2022-11-30 00:52:35 +0000

    add eslint and fix button

--- d3.js
+++ d3.js
@@ -5,64 +5,74 @@
 //     console.log(data);
 // })
 
-// set the dimensions and margins of the graph 
+// set the dimensions and margins of the graph
 
-const margin = {top: 80, right: 60, bottom: 100, left: 60},
-    width = 480 - margin.left - margin.right,
-    height = 400 - margin.top - margin.bottom;
+const margin = { top: 80, right: 60, bottom: 100, left: 60 };
+const width = 1200 - margin.left - margin.right;
+const height = 1000 - margin.top - margin.bottom;
 
 // append the svg object to the body of the page
-const svg = d3.select("#my_dataviz")
-  .append("svg")
-    .attr("width", width + margin.left + margin.right)
-    .attr("height", height + margin.top + margin.bottom)
-  .append("g")
-    .attr("transform", `translate(${margin.left}, ${margin.top})`);
+const svg = d3.select('#my_dataviz')
+  .append('svg')
+  .attr('width', width + margin.left + margin.right)
+  .attr('height', height + margin.top + margin.bottom)
+  .append('g')
+  .attr('transform', `translate(${margin.left}, ${margin.top})`);
 
 // Parse the Data
-d3.csv("/dataset/artworks_class.csv").then ( function(data) {
+d3.csv('/dataset/artworks.csv').then(function (data) {
+  const result = [];
+
+  data.forEach(function (a) {
+    if (!this[a.Classification]) {
+        this[a.Classification] = {
+            Classification: a.Classification,
+            orders: 0
+        };
+        result.push(this[a.Classification]);
+    }
+    this[a.Classification].orders += 1;
+}, Object.create(null));
 
   // sort data
-  data.sort(function(b, a) {
-    return a.Value - b.Value;
+  result.sort(function (b, a) {
+    return a.orders - b.orders;
   });
-
   // X axis
   const x = d3.scaleBand()
-    .range([ 0, width ])
-    .domain(data.map(d => d.Classification))
+    .range([0, width])
+    .domain(result.map(d => d.Classification))
     .padding(0.2);
-  svg.append("g")
-    .attr("transform", `translate(0, ${height})`)
+  svg.append('g')
+    .attr('transform', `translate(0, ${height})`)
     .call(d3.axisBottom(x))
-    .selectAll("text")
-      .attr("transform", "translate(-10,0)rotate(-45)")
-      .style("text-anchor", "end");
+    .selectAll('text')
+    .attr('transform', 'translate(-10,0)rotate(-45)')
+    .style('text-anchor', 'end');
 
   // Add Y axis
   const y = d3.scaleLinear()
     .domain([0, 35000])
-    .range([ height, 0]);
-  svg.append("g")
+    .range([height, 0]);
+  svg.append('g')
     .call(d3.axisLeft(y));
 
   // Bars
-  svg.selectAll("mybar")
-    .data(data)
+  svg.selectAll('mybar')
+    .data(result)
     .enter()
-    .append("rect")
-      .attr("x", d => x(d.Classification))
-      .attr("y", d => y(d.Value))
-      .attr("width", x.bandwidth())
-      .attr("height", d => height - y(d.Value))
-      .attr("fill", "#69b3a2")
+    .append('rect')
+    .attr('x', d => x(d.Classification))
+    .attr('y', d => y(d.orders))
+    .attr('width', x.bandwidth())
+    .attr('height', d => height - y(d.orders))
+    .attr('fill', '#69b3a2');
 
-      svg.append("text")
-      .attr("x", (width / 2))             
-      .attr("y", 0 - (margin.top / 2))
-      .attr("text-anchor", "middle")  
-      .style("font-size", "16px") 
-      .style("text-decoration", "underline")  
-      .text("Top catorgeries of artworks in the museum");
-
-})
+  svg.append('text')
+    .attr('x', (width / 2))
+    .attr('y', 0 - (margin.top / 2))
+    .attr('text-anchor', 'middle')
+    .style('font-size', '16px')
+    .style('text-decoration', 'underline')
+    .text('Top catorgeries of artworks in the museum');
+});
--- javascript.js
+++ javascript.js
@@ -1,47 +1,64 @@
-const Arts_Stats = document.getElementById("artworks_stats")
+const artsStats = document.getElementById('my_dataviz');
 
-const btn = document.getElementById("press")
+const artistsStats = document.getElementById('circular_barchart');
 
-btn.onclick = function () {
-   if (Arts_Stats.style.display !== "none") {
-    Arts_Stats.style.display = "none";
-   } else {
-    Arts_Stats.style.display = "block"
-   }
-  };
+console.log(artistsStats);
 
-const reply = document.getElementById("contact_submit")
+console.log(artsStats);
 
-reply.onclick = function () {
-  window.alert("Thank you for leaving a message! You will be replied very soon")
+const btn1 = document.getElementById('class_btn');
+
+const btn2 = document.getElementById('nationality_btn');
+
+btn1.onclick = function () {
+  if (artsStats.style.display !== 'block') {
+    artsStats.style.display = 'block';
+  } else {
+    artsStats.style.display = 'none';
+  }
 };
 
-//slideshow function from w3school: https://www.w3schools.com/howto/howto_js_slideshow.asp
+btn2.onclick = function () {
+  if (artistsStats.style.display !== 'block') {
+    artistsStats.style.display = 'block';
+  } else {
+    artistsStats.style.display = 'none';
+  }
+};
+const reply = document.getElementById('contact_submit');
+// const emailAddress = document.getElementById('exampleInputEmail1');
+// const comments = document.getElementById('exampleInputComment');
+
+reply.onclick = function () {
+    window.alert('Thank you for leaving a message! You will be replied very soon');
+};
+
+// slideshow function from w3school: https://www.w3schools.com/howto/howto_js_slideshow.asp
 let slideIndex = 1;
 showSlides(slideIndex);
 
 // Next/previous controls
-function plusSlides(n) {
+function plusSlides (n) {
   showSlides(slideIndex += n);
 }
 
 // Thumbnail image controls
-function currentSlide(n) {
+function currentSlide (n) {
   showSlides(slideIndex = n);
 }
 
-function showSlides(n) {
+function showSlides (n) {
   let i;
-  let slides = document.getElementsByClassName("mySlides");
-  let dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
+  const slides = document.getElementsByClassName('mySlides');
+  const dots = document.getElementsByClassName('dot');
+  if (n > slides.length) { slideIndex = 1; }
+  if (n < 1) { slideIndex = slides.length; }
   for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
+    slides[i].style.display = 'none';
   }
   for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
+    dots[i].className = dots[i].className.replace(' active', '');
   }
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
+  slides[slideIndex - 1].style.display = 'block';
+  dots[slideIndex - 1].className += ' active';
 }
